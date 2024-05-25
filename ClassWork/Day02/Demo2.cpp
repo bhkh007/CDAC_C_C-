7c9189e28200d639056139331d57fbc9f1d5cec7
bhkh007 <khandarbhavesh007@gmail.com>
2024-05-26 00:02:58 +0530

    another classwork

---
+++ ClassWork/Day02/Demo2.cpp
@@ -0,0 +1,21 @@
+
+
+#include <cstdio>
+#include <string.h>
+
+class Employee{
+    public:
+    int EmpId;
+    float Salary;
+
+
+};
+
+int main(){
+
+    Employee emp;
+    emp.EmpId=100;
+    printf("%d\n",emp.EmpId);
+}
+
+
